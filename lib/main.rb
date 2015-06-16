70d39da8569493c454054c3472d658b67769207c
Miłosz <cookieburra@localhost.localdomain>
2015-06-16 20:53:32 +0200

    Everything works, but is crazy ugly.

--- lib/main.rb
+++ lib/main.rb
@@ -22,33 +22,25 @@ require 'gtk3'
 
 Gtk.init
 
-# No documentation for now
+# TODO: No documentation for now
 class ProgramWindow < Gtk::Window
-  # Set global image objects for each cell type (alive/dead)
-  parent_directory = File.expand_path('..', File.dirname(__FILE__))
-  @IMG_CELL_ALIVE = Gdk::Pixbuf.new(file:
-                                    parent_directory + '/assets/circle.png')
-
-  @IMG_CELL_DEAD = Gdk::Pixbuf.new(file:
-                                   parent_directory + '/assets/cross.png')
 
   def initialize(game)
     super()
 
     self.title = 'Conway\'s game of life'
-    # Not reallt needed, since set_resizable invalidates it
+    # Not really needed, since set_resizable invalidates it
     # set_size_request(300, 300)
     set_resizable(false)
 
     # Set global image objects for each cell type (alive/dead)
     parent_directory = File.expand_path('..', File.dirname(__FILE__))
-    @IMG_CELL_ALIVE = Gdk::Pixbuf.new(file:
+    @Img_cell_alive = Gdk::Pixbuf.new(file:
                                       parent_directory + '/assets/circle.png')
 
-    @IMG_CELL_DEAD = Gdk::Pixbuf.new(file:
+    @Img_cell_dead = Gdk::Pixbuf.new(file:
                                      parent_directory + '/assets/cross.png')
 
-    # Save the world ;)
     @game = game
 
     set_up_gui
@@ -58,8 +50,8 @@ class ProgramWindow < Gtk::Window
   end
 
   def set_up_gui
-    # Keep those to refresh cells during operation
-    layout_grid = Gtk::Grid.new
+    # Keep the widgets we need
+    @layout_grid = Gtk::Grid.new
 
     @cell_grid = get_new_cell_grid
     @cell_alignment = Gtk::Alignment.new(0.5, 0.5, 0, 0)
@@ -78,36 +70,60 @@ class ProgramWindow < Gtk::Window
     # We should center the cell_grid
     @cell_alignment << @cell_grid
 
-    layout_grid.attach(@cell_alignment, 0, 0, x, y)
-    layout_grid.attach(@button_next_gen, 0, y, x - 1, 1)
-    layout_grid.attach(@button_reset, x - 1, y, 1, 1)
-    layout_grid.attach(@button_loop, 0, y + 1, x, 1)
+    @layout_grid.attach(@cell_alignment, 0, 0, x, y)
+    @layout_grid.attach(@button_next_gen, 0, y, x - 1, 1)
+    @layout_grid.attach(@button_reset, x - 1, y, 1, 1)
+    @layout_grid.attach(@button_loop, 0, y + 1, x, 1)
 
-    layout_grid.expand = true
-    add(layout_grid)
+    @layout_grid.expand = true
+    add(@layout_grid)
 
   end
+	
+  def next_gen
+    @game.next_generation! 
+    @cell_grid.destroy
+    @cell_grid = get_new_cell_grid
+    @cell_grid.show_all
+    @cell_alignment << @cell_grid
+  end
+
+  def set_action(disable)
+    [@button_next_gen, @button_loop].each do |button|
+      button.set_sensitive(disable)
+    end
+  end
+
+  def cell_reset
+    @game.reset! 
+    @game.cells.add_life!(8)
+    @cell_grid.destroy
+    @cell_grid = get_new_cell_grid
+    @cell_grid.show_all
+    @cell_alignment << @cell_grid
+    set_action(true)
+  end
+
 
   def set_up_callbacks
     signal_connect('destroy') { Gtk.main_quit }
 
     @button_next_gen.signal_connect('clicked') do
-      @game.next_generation! # Could be moved to a named method
-      @cell_grid.destroy
-      @cell_grid = get_new_cell_grid
-      @cell_grid.show_all
-      @cell_alignment << @cell_grid
-      @button_next_gen.set_sensitive(false) if @game.cells.total_lives == 0
+      next_gen
+      set_action(false) if @game.cells.total_lives == 0
     end
 
-    @button_reset.signal_connect('clicked') do
-      @game.reset! # Could be moved to a named method
-      @game.cells.add_life!(8)
-      @cell_grid.destroy
-      @cell_grid = get_new_cell_grid
-      @cell_grid.show_all
-      @cell_alignment << @cell_grid
-      @button_next_gen.set_sensitive(true)
+    @button_reset.signal_connect('clicked') { cell_reset }
+
+    @button_loop.signal_connect('clicked') do
+      GLib::Timeout.add_seconds(1) do
+        next_gen
+        if @game.cells.total_lives == 0 
+          set_action(false) if @game.cells.total_lives == 0
+          false
+        else true 
+        end
+      end
     end
   end
 
@@ -115,7 +131,7 @@ class ProgramWindow < Gtk::Window
     new_cell_grid = Gtk::Grid.new
     
     @game.cells.every_cell do |x, y|
-      cell_pixbuf = @game.cells.alive?(x, y) ? @IMG_CELL_ALIVE : @IMG_CELL_DEAD
+      cell_pixbuf = @game.cells.alive?(x, y) ? @Img_cell_alive : @Img_cell_dead
       cell = Gtk::Image.new(pixbuf: cell_pixbuf)
       new_cell_grid.attach(cell, x, y, 1, 1)
     end
@@ -124,6 +140,9 @@ class ProgramWindow < Gtk::Window
   end
 
 end # End of class ProgramWindow
+
+# TODO: Get all of starting vars from a dialog for the user
+# Hint: Number of those variables is one (edge size)
 
 game = Conway::Game.new(15)
 game.cells.add_life!(80)
