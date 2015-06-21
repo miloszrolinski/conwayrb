76e7c7d947f021757f18a752a20c082ec2d9a811
Miłosz <cookieburra@localhost.localdomain>
2015-06-21 20:13:52 +0200

    Placeholder file in place

---
+++ lib/settings.rb
@@ -0,0 +1,23 @@
+#    This file is part of conwayrb.
+#
+#    conwayrb is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    conwayrb is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with conwayrb.  If not, see <http://www.gnu.org/licenses/>.
+
+module Conway
+  module Interface
+
+
+
+
+  end # ... of module Interface
+end # ... of module Conway
